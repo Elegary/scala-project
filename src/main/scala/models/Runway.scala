42a6902b5fe6d36690e6232a5c5d40dbcb7f141a
RaphaelBellepeau <raphael.bellepeau@gmail.com>
2024-12-18 22:08:48 +0100

    Added runway repostitory and model support for scalikedjbc

--- src/main/scala/models/Runway.scala
+++ src/main/scala/models/Runway.scala
@@ -1,6 +1,7 @@
 package models
 
 import parser.CsvParser
+import scalikejdbc.{SQLSyntaxSupport, WrappedResultSet}
 
 // Model for Runways
 case class Runway(
@@ -26,7 +27,31 @@ case class Runway(
                    heDisplacedThresholdFt: Option[Int]
                  )
 
-object Runway {
+object Runway extends SQLSyntaxSupport[Runway] {
+
+  def apply(rs: WrappedResultSet): Runway = new Runway(
+    id = rs.int("id"),
+    airportRef = rs.int("airport_ref"),
+    airportIdent = rs.string("airport_ident"),
+    lengthFt = rs.intOpt("length_ft"),
+    widthFt = rs.intOpt("width_ft"),
+    surface = rs.stringOpt("surface"),
+    lighted = rs.boolean("lighted"),
+    closed = rs.boolean("closed"),
+    leIdent = rs.stringOpt("le_ident"),
+    leLatitudeDeg = rs.doubleOpt("le_latitude_deg"),
+    leLongitudeDeg = rs.doubleOpt("le_longitude_deg"),
+    leElevationFt = rs.intOpt("le_elevation_ft"),
+    leHeadingDegT = rs.doubleOpt("le_heading_degT"),
+    leDisplacedThresholdFt = rs.intOpt("le_displaced_threshold_ft"),
+    heIdent = rs.stringOpt("he_ident"),
+    heLatitudeDeg = rs.doubleOpt("he_latitude_deg"),
+    heLongitudeDeg = rs.doubleOpt("he_longitude_deg"),
+    heElevationFt = rs.intOpt("he_elevation_ft"),
+    heHeadingDegT = rs.doubleOpt("he_heading_degT"),
+    heDisplacedThresholdFt = rs.intOpt("he_displaced_threshold_ft")
+  )
+
   def from(csvLine: String): Runway = {
     val fields = CsvParser.splitCsvLine(csvLine)
 
