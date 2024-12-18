3e21a6bbaea83324b063a857d0cff7a450f696ef
RaphaelBellepeau <raphael.bellepeau@gmail.com>
2024-12-18 20:47:56 +0100

    Changed way to read files: use stream to avoid big files in memory

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,36 +1,41 @@
 import models.{Airport, Country, Runway}
-import repository.DBSetup
+import os.Path
+import repository.{AirportRepository, DBSetup}
 import scalikejdbc._
 import scalikejdbc.config._
 
 // DBs.setup/DBs.setupAll loads specified JDBC driver classes.
 
+
 object Main {
   def main(args: Array[String]): Unit = {
     println("BDML Project")
 
+    val data_path = os.pwd / "data"
+
     DBSetup.initialize()
+
+    load_airports(data_path / "airports.csv")
+    load_countries(data_path / "countries.csv")
 
   }
 
-  def load_files(): Unit = {
-    val path = os.pwd / "data"
-    val airports_lines = os.read.lines(path / "airports.csv")
+  def load_airports(path: Path): Unit = {
+    val airportRepo = new AirportRepository()
+    airportRepo.createTable()
+    val airports_lines = os.read.lines.stream(path)
     airports_lines.drop(1).foreach(airport_line => {
       val airport = Airport.from(airport_line)
-      println(airport)
+      airportRepo.insert(airport)
     })
+  }
 
-    val countries_lines = os.read.lines(path / "countries.csv")
+  def load_countries(path: Path): Unit = {
+    val countries_lines = os.read.lines(path)
     countries_lines.drop(1).foreach(country_line => {
       val country = Country.from(country_line)
       println(country)
     })
-
-    val runways_lines = os.read.lines(path / "runways.csv")
-    runways_lines.drop(1).foreach(runway_line => {
-      val runway = Runway.from(runway_line)
-      println(runway)
-    })
   }
+  
 }
