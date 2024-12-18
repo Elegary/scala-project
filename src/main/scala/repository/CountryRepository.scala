2d5b3b3c1ffb7829736ee9dab37efd8c110e86d2
Papy <21334807+Elegary@users.noreply.github.com>
2024-12-18 20:52:25 +0100

    Added country repostitory and model support for scalikedjbc

---
+++ src/main/scala/repository/CountryRepository.scala
@@ -0,0 +1,43 @@
+package repository
+
+import models.Country
+
+import scalikejdbc._
+
+
+class CountryRepository {
+
+  def createTable(): Unit = {
+    DB autoCommit { implicit session =>
+      sql"""
+      CREATE TABLE IF NOT EXISTS countries (
+        id INT PRIMARY KEY,
+        code VARCHAR(255),
+        name VARCHAR(255),
+        continent VARCHAR(255),
+        wikipedia_link VARCHAR(1000),
+        keywords VARCHAR(1000)
+      )
+      """.execute.apply()
+    }
+  }
+
+  def insert(country: Country): Unit = {
+    DB localTx { implicit session =>
+      sql"""
+      INSERT INTO countries (id, code, name, continent, wikipedia_link, keywords)
+      VALUES (
+        ${country.id}, ${country.code}, ${country.name}, ${country.continent},
+        ${country.wikipediaLink}, ${country.keywords.orNull}
+      )
+      """.update.apply()
+    }
+  }
+
+  def findAll(): List[Country] = {
+    DB readOnly { implicit session =>
+      sql"SELECT * FROM countries".map(Country.apply).list.apply()
+    }
+  }
+
+}
