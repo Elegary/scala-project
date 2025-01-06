021021780bc7d2c3ffb6ce34386a4619b1b852b5
RaphaelBellepeau <raphael.bellepeau@gmail.com>
2025-01-06 20:16:27 +0100

    Added partial matching

--- src/main/scala/utils/Query.scala
+++ src/main/scala/utils/Query.scala
@@ -9,7 +9,7 @@ object Query {
   // get list of airports and runways by country code
   // structure is List with airport_name: String, Runways: List[Runway]
   def queryAirportsAndRunwaysByCountryCode(code: String): Seq[AirportRunways] = {
-    val runways = new RunwayRepository().getAirportsAndRunwaysByCountryCode(code)
+    val runways = RunwayRepository.getAirportsAndRunwaysByCountryCode(code)
     runways.groupBy(_._1).map(airport => {
       AirportRunways(airport._1, airport._2.map(_._2).toList)
     }).toSeq
@@ -17,11 +17,25 @@ object Query {
 
   // get list of airports and runways by partial country name
   // structure is List with airport_name: String, Runways: List[Runway]
-  def queryAirportsAndRunwaysByPartialCountryName(name: String): Seq[AirportRunways] = {
-    val runways = new RunwayRepository().getAirportsAndRunwaysByPartialCountryName(name)
-    runways.groupBy(_._1).map(airport => {
-      AirportRunways(airport._1, airport._2.map(_._2).toList)
-    }).toSeq
+  def queryAirportsAndRunwaysByPartialCountryName(partialName: String): Option[(String, Seq[AirportRunways])] = {
+
+    // Retrieve data using the repository method
+    val runwaysData: List[(String, String, Runway)] = RunwayRepository.getAirportsAndRunwaysByPartialCountryName(partialName)
+
+    // If the list is not empty, proceed to process the data
+    runwaysData.headOption.map { firstEntry =>
+      // Extract the country name from the first entry
+      val countryName = firstEntry._1
+
+      // Group the data by airport name
+      val airportsWithRunways = runwaysData.groupBy(_._2).map { case (airportName, entries) =>
+        // Create a list of runways for each airport
+        AirportRunways(airportName, entries.map(_._3))
+      }.toSeq
+
+      // Return the country name and the list of AirportRunways
+      (countryName, airportsWithRunways)
+    }
   }
 
 }
