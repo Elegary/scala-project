bd3281f88467ddc0a8ec52c7e533dd3397a23af4
Papy <21334807+Elegary@users.noreply.github.com>
2025-01-06 20:16:56 +0100

    Simplified main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,91 +1,18 @@
 import db.DatabaseSetup
-import models.{Airport, Country, Runway}
-import os.Path
-import repository.{AirportRepository, CountryRepository, DBSetup, RunwayRepository}
-import scalikejdbc._
-import scalikejdbc.config._
-
 import api.ApiService
-
-// DBs.setup/DBs.setupAll loads specified JDBC driver classes.
+import ui.ConsoleApp
 
 
 object Main {
   def main(args: Array[String]): Unit = {
     println("BDML Project")
 
-    val data_path = os.pwd / "data"
-
     DatabaseSetup.init()
 
     ApiService.start()
 
-    println("Loading airports...")
-    load_airports(data_path / "airports.csv")
-    println("Loading countries...")
-    load_countries(data_path / "countries.csv")
-    println("Loading runways...")
-    load_runways(data_path / "runways.csv")
+    ConsoleApp.userInterface()
 
-    println("Done!")
-
-    while (true) {
-      println("Enter a country code")
-      val country_code = scala.io.StdIn.readLine()
-      val allCountries = new CountryRepository().findAll()
-      val country = allCountries.find(_.code == country_code)
-      println(country)
-      val runways = new RunwayRepository().getAiportsAndRunwaysByCountryCode(country_code)
-      runways.groupBy(_._1).map(airport => {
-        println(s"Airport: ${airport._1}")
-        airport._2.foreach(runway => println(s"\t- ${runway._2.niceDisplay}"))
-      })
-      val tenHighestAirports = AirportRepository.getAirportsCountByCountry(10, ascending = false)
-      tenHighestAirports.foreach(println)
-      val tenLowestAirports = AirportRepository.getAirportsCountByCountry(10, ascending = true)
-      tenLowestAirports.foreach(println)
-      val typesOfRunways = new RunwayRepository().getTypeOfRunwaysPerCountry()
-      // result is a list of tuples (country_name, runway_surface), we group by country name
-      val groupedByCountry = typesOfRunways.groupBy(_._1)
-      // we print the results
-      groupedByCountry.map(country => {
-        println(s"Country: ${country._1}")
-        country._2.foreach(runway => println(s"\t- ${runway._2}"))
-      })
-
-      val runwaysLatitudes = new RunwayRepository().getTop10RunwayLatitudes()
-      runwaysLatitudes.foreach(println)
-    }
-
-  }
-
-  def load_airports(path: Path): Unit = {
-    AirportRepository.createTable()
-    val airports_lines = os.read.lines.stream(path)
-    airports_lines.drop(1).foreach(airport_line => {
-      val airport = Airport.from(airport_line)
-      AirportRepository.insert(airport)
-    })
-  }
-
-  def load_countries(path: Path): Unit = {
-    val countriesRepo = new CountryRepository()
-    countriesRepo.createTable()
-    val countries_lines = os.read.lines(path)
-    countries_lines.drop(1).foreach(country_line => {
-      val country = Country.from(country_line)
-      countriesRepo.insert(country)
-    })
-  }
-
-  def load_runways(path: Path): Unit = {
-    val runwaysRepo = new RunwayRepository()
-    runwaysRepo.createTable()
-    val runways_lines = os.read.lines(path)
-    runways_lines.drop(1).foreach(runway_line => {
-      val runway = Runway.from(runway_line)
-      runwaysRepo.insert(runway)
-    })
   }
 
 }
--- src/main/scala/db/DatabaseSetup.scala
+++ src/main/scala/db/DatabaseSetup.scala
@@ -1,5 +1,8 @@
 package db
 
+import models.{Airport, Country, Runway}
+import os.Path
+import repository.{AirportRepository, CountryRepository, RunwayRepository}
 import scalikejdbc._
 import scalikejdbc.config._
 
@@ -10,5 +13,45 @@ object DatabaseSetup {
     Class.forName("org.h2.Driver")
     DBs.setupAll()
     // Set global settings if needed
+
+    val data_path = os.pwd / "data"
+
+    println("Loading airports...")
+    load_airports(data_path / "airports.csv")
+    println("Loading countries...")
+    load_countries(data_path / "countries.csv")
+    println("Loading runways...")
+    load_runways(data_path / "runways.csv")
+
+    println("Done!")
+
   }
+
+  def load_airports(path: Path): Unit = {
+    AirportRepository.createTable()
+    val airports_lines = os.read.lines.stream(path)
+    airports_lines.drop(1).foreach(airport_line => {
+      val airport = Airport.from(airport_line)
+      AirportRepository.insert(airport)
+    })
+  }
+
+  def load_countries(path: Path): Unit = {
+    CountryRepository.createTable()
+    val countries_lines = os.read.lines(path)
+    countries_lines.drop(1).foreach(country_line => {
+      val country = Country.from(country_line)
+      CountryRepository.insert(country)
+    })
+  }
+
+  def load_runways(path: Path): Unit = {
+    RunwayRepository.createTable()
+    val runways_lines = os.read.lines(path)
+    runways_lines.drop(1).foreach(runway_line => {
+      val runway = Runway.from(runway_line)
+      RunwayRepository.insert(runway)
+    })
+  }
+
 }
